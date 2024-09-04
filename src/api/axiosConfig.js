609f9cedef944baf8aebbfb62d7bb31f77dd987b
Pranav Jagadeesha <115452266+PRANAVJ2804@users.noreply.github.com>
2024-09-04 20:34:14 +0530

    Add files via upload

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://movies-4q5d.onrender.com",
+  baseURL: "https://movies-bh3z.onrender.com",
   timeout: 20000,
   headers: { "Content-type": "application/json" },
 });
